13f4d84bcbc61692d71ff25e5df6a61c31e662d5
diegodavila <djr618@lehigh.edu>
2014-08-27 16:50:21 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+//Diego Davila
+//Lab01-HelloWorld
+
+//Class: 
+public class HelloWorld{
+    
+    //Main method
+    public static void main(String[] args) {
+        
+        //Print statement with box around it
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("#                         #");
+        System.out.println("#    Hello World 2014!!   #");
+        System.out.println("#                         #");
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("test");
+    }
+}
