3e878ab8080fd5016a172977c0a934644d8352a0
diegodavila <djr618@lehigh.edu>
2014-08-28 01:44:57 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+// Diego Davila
+// Aug. 27 2014
+// hw01 Welcome Class
+//CSE2
+
+//Class:
+public class WelcomeClass{
+    
+    //Main Method
+    public static void main(String[] args) {
+        
+        //Print welcome on top with Lehigh ID 
+        // djr618 in the middle
+        // "\\" inserts a backslash at this point (Escape Sequence)
+        
+        System.out.println("    -----------    ");
+        System.out.println("    | Welcome |    ");
+        System.out.println("    -----------    ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-D--J--R--6--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("I am from El Salvador");
+        System.out.println("and I'm excited to be");
+        System.out.println("in college in the US! ");
+    }
+}
